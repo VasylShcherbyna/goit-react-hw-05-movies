f5cd0c9592152afb4fc2739ac61e6aad5d330ef6
VasylShcherbyna <75271842+VasylShcherbyna@users.noreply.github.com>
2021-12-05 20:13:23 +0100

    Updates

---
+++ static/js/main.99ddfc32.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-05-movies"]=this["webpackJsonpgoit-react-hw-05-movies"]||[]).push([[0],{11:function(t,e,n){"use strict";n.r(e);var c=n(0),i=n.n(c),o=n(3),s=n.n(o),r=(n(8),n(9),n(1));var a=function(){return Object(r.jsx)("div",{className:"App"})};s.a.render(Object(r.jsx)(i.a.StrictMode,{children:Object(r.jsx)(a,{})}),document.getElementById("root"))},8:function(t,e,n){},9:function(t,e,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.99ddfc32.chunk.js.map
