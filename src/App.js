dc80ed4fcd81ad3e4cc7772ba3b78f420e6cc674
VasylShcherbyna <75271842+VasylShcherbyna@users.noreply.github.com>
2021-12-08 21:18:10 +0100

    router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import React from "react";
 import { Route, NavLink, Switch } from "react-router-dom";
 import "./App.css";
+import HomePageView from "./views/HomePageView";
+import MoviesPageView from "./views/MoviesPageView";
 
 function App() {
-  return <div className="App"></div>;
+  <>
+    HomePageView
+    <ul>
+      <li>
+        <NavLink
+          exact
+          to="/"
+          className="NavLink"
+          activeClassName="NavLink--active"
+        >
+          HomePage
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/authors"
+          className="NavLink"
+          activeClassName="NavLink--active"
+        >
+          MoviesPage
+        </NavLink>
+      </li>
+    </ul>
+    <Switch>
+      <Route exact path="/" component={HomePageView} />
+      <Route exact path="/books" component={MoviesPageView} />
+    </Switch>
+  </>;
 }
 
 export default App;
